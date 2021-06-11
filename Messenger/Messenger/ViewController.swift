2aeb01e28ff67d000f66f581d8b0f02d4c37815d
Jervy Umandap <mac@Jervys-MacBook-Pro.local>
2021-06-11 17:20:57 +0800

    Starter project

---
+++ Messenger/Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Jervy Umandap on 6/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
